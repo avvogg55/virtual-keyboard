7e0eea17e1926c6d2ebfc8a34f2daf9be7045510
Gleb Goncharov <avvogg55@gmail.com>
2023-05-01 23:14:35 +0300

    feat: add input functionality and backspace

--- script.js
+++ script.js
@@ -19,19 +19,21 @@ function appendButtons(i, line) {
   const lBT = 'tab-style';
   const lBM = 'medium';
   const lBSpace = 'space-style';
-  const notInputBtn = 'not-input-btn';
+
   const inputBtn = 'input-btn';
+  const refBtn = 'ref-btn';
+  const functBtn = 'funct-btn';
 
   const firstText = ['~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'BACKSPC'];
-  const secondText = ['TAB', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '&bsol;', 'DEL'];
-  const thirdText = ['CAPSLOCK', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '&apos;', 'ENTER'];
-  const fourthText = ['SHIFT', '&bsol;', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'UP', 'SHFT'];
+  const secondText = ['TAB', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '&bsol;', 'DEL'];
+  const thirdText = ['CAPSLCK', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '&apos;', 'ENTER'];
+  const fourthText = ['SHIFT', '&bsol;', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'UP', 'SHFT'];
   const fifthText = ['CTRL', 'WIN', 'ALT', 'SPACE', 'ALT', 'CTRL', 'LFT', 'DWN', 'RGT'];
 
-  const firstId = ['tilda', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'minus', 'equality-sign', 'backspace'];
-  const secondId = ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'left-square-bracket', 'right-square-bracket', 'left-slash', 'delete'];
-  const thirdId = ['caps-lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'semicolon', 'single-quote', 'enter'];
-  const fourthId = ['left-shift', 'left-slash-single', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'comma', 'dot', 'right-slash', 'up', 'right-shift'];
+  const firstId = ['tilda', '1-button', '2-button', '3-button', '4-button', '5-button', '6-button', '7-button', '8-button', '9-button', '0-button', 'minus', 'equality-sign', 'backspace'];
+  const secondId = ['tab', 'q-button', 'w-button', 'e-button', 'r-button', 't-button', 'y-button', 'u-button', 'i-button', 'o-button', 'p-button', 'left-square-bracket', 'right-square-bracket', 'left-slash', 'delete'];
+  const thirdId = ['caps-lock', 'a-button', 's-button', 'd-button', 'f-button', 'g-button', 'h-button', 'j-button', 'k-button', 'l-button', 'semicolon', 'single-quote', 'enter'];
+  const fourthId = ['left-shift', 'left-slash-single', 'z-button', 'x-button', 'c-button', 'v-button', 'b-button', 'n-button', 'm-button', 'comma', 'dot', 'right-slash', 'up', 'right-shift'];
   const fifthId = ['left-ctrl', 'win', 'left-alt', 'space', 'right-alt', 'right-ctrl', 'left', 'down', 'right'];
 
   const firstClass = [lBS, lBS, lBS, lBS, lBS, lBS, lBS, lBS, lBS, lBS, lBS, lBS, lBS, lBL];
@@ -41,15 +43,15 @@ function appendButtons(i, line) {
   const fifthClass = [lBM, lBS, lBM, lBSpace, lBS, lBM, lBS, lBS, lBS];
 
   const firstInput = [inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn,
-    inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn];
-  const secondInput = [inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn,
-    inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn];
-  const thirdInput = [notInputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn,
-    inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn];
-  const fourthInput = [notInputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn,
-    inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, notInputBtn, notInputBtn];
-  const fifthInput = [notInputBtn, notInputBtn, notInputBtn, inputBtn, notInputBtn,
-    notInputBtn, notInputBtn, notInputBtn, notInputBtn];
+    inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, refBtn];
+  const secondInput = [refBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn,
+    inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, refBtn];
+  const thirdInput = [functBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn,
+    inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, refBtn];
+  const fourthInput = [functBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn,
+    inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, inputBtn, functBtn, functBtn];
+  const fifthInput = [functBtn, functBtn, functBtn, refBtn, functBtn,
+    functBtn, functBtn, functBtn, functBtn];
 
   if (i === 0) {
     for (let y = 0; y < 14; y += 1) {
@@ -135,8 +137,150 @@ function appendLines(parent) {
 
 appendLines(keyboardBlock);
 
+const buttonIds = {
+  ArrowUp: 'up',
+  ArrowDown: 'down',
+  ArrowLeft: 'left',
+  ArrowRight: 'right',
+  Alt: 'left-alt',
+  AltGraph: 'right-alt',
+  ' ': 'space',
+  Win: 'win',
+  OS: 'win',
+  ControlRight: 'right-ctrl',
+  ControlLeft: 'left-ctrl',
+  Backspace: 'backspace',
+  Tab: 'tab',
+  Delete: 'delete',
+  CapsLock: 'caps-lock',
+  Enter: 'enter',
+  1: '1-button',
+  2: '2-button',
+  3: '3-button',
+  4: '4-button',
+  5: '5-button',
+  6: '6-button',
+  7: '7-button',
+  8: '8-button',
+  9: '9-button',
+  0: '0-button',
+  '=': 'equality-sign',
+  '~': 'tilda',
+  '-': 'minus',
+  '[': 'left-square-bracket',
+  ']': 'right-square-bracket',
+  '\\': 'left-slash',
+  ';': 'semicolon',
+  SingleQuote: 'single-quote',
+  ',': 'comma',
+  '.': 'dot',
+  '/': 'right-slash',
+  q: 'q-button',
+  w: 'w-button',
+  e: 'e-button',
+  r: 'r-button',
+  t: 't-button',
+  y: 'y-button',
+  u: 'u-button',
+  i: 'i-button',
+  o: 'o-button',
+  p: 'p-button',
+  a: 'a-button',
+  s: 's-button',
+  d: 'd-button',
+  f: 'f-button',
+  g: 'g-button',
+  h: 'h-button',
+  j: 'j-button',
+  k: 'k-button',
+  l: 'l-button',
+  z: 'z-button',
+  x: 'x-button',
+  c: 'c-button',
+  v: 'v-button',
+  b: 'b-button',
+  n: 'n-button',
+  m: 'm-button',
+};
+
+//----------------Click listeners---------------------
+
 keyboardBlock.addEventListener('click', (event) => {
   if (event.target.classList.contains('input-btn')) {
     textArea.value += event.target.innerHTML;
+  }
+});
+
+keyboardBlock.addEventListener('click', (event) => {
+  if (event.target.id === 'backspace') {
+    console.log('backsp click');
+    const currentValue = textArea.value;
+    textArea.value = currentValue.substring(0, currentValue.length - 1);
+  }
+});
+
+//-----------------Key Listeners----------------------
+
+document.addEventListener('keydown', (event) => {
+  event.preventDefault();
+  const buttonId = buttonIds[event.key];
+  if (buttonId) {
+    const button = document.getElementById(buttonId);
+    if (button.classList.contains('input-btn')) {
+      button.classList.add('active-button');
+      button.click();
+    }
+  }
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowLeft') {
+    textArea.setSelectionRange(textArea.selectionStart - 1, textArea.selectionStart - 1);
+    const buttonId = buttonIds[event.key];
+    const button = document.getElementById(buttonId);
+    button.classList.add('active-button');
+  }
+
+  if (event.key === 'ArrowRight') {
+    textArea.setSelectionRange(textArea.selectionStart + 1, textArea.selectionStart + 1);
+    const buttonId = buttonIds[event.key];
+    const button = document.getElementById(buttonId);
+    button.classList.add('active-button');
+  }
+
+  if (event.key === 'ArrowUp') {
+    textArea.setSelectionRange(textArea.selectionStart - textArea.cols,
+      textArea.selectionStart - textArea.cols);
+    const buttonId = buttonIds[event.key];
+    const button = document.getElementById(buttonId);
+    button.classList.add('active-button');
+  }
+  if (event.key === 'ArrowDown') {
+    textArea.setSelectionRange(textArea.selectionStart + textArea.cols,
+      textArea.selectionStart + textArea.cols);
+    const buttonId = buttonIds[event.key];
+    const button = document.getElementById(buttonId);
+    button.classList.add('active-button');
+  }
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Backspace') {
+    const buttonId = buttonIds[event.key];
+    const button = document.getElementById(buttonId);
+    button.classList.add('active-button');
+    const currentValue = textArea.value;
+    textArea.value = currentValue.substring(0, currentValue.length - 1);
+    event.preventDefault();
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  const buttonId = buttonIds[event.key];
+  if (buttonId) {
+    const button = document.getElementById(buttonId);
+    if (button) {
+      button.classList.remove('active-button');
+    }
   }
 });
